02902e9c4f197c25ee6fe3fa264214a95701d15e
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-08-17 13:56:04 +0300

    Task-2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,35 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// 1. First version
+
+const elements = []
+
+for (let i = 0; i < ingredients.length; i++) {
+  const element = ingredients[i];
+
+  const itemElement = document.createElement('li');
+  itemElement.classList.add('item');
+  itemElement.textContent = element;
+
+  elements.push(itemElement);  
+}
+
+const listElelement = document.querySelector('ul');
+listElelement.append(...elements);
+console.log(listElelement);
+
+// 2. Second Version
+
+// const createItemEl = ingredients.map(ingredient => {
+//   const itemEl = document.createElement('li');
+//   itemEl.classList.add('item');
+//   itemEl.textContent = ingredient;
+//   return itemEl;
+// })
+
+// const listEl = document.querySelector('ul');
+// listEl.append(...createItemEl);
+// console.log(listEl);
+
