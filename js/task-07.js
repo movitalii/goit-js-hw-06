30f3e6fc5001551a252a92bc1fb74d059b171aac
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-08-19 20:52:10 +0300

    Task-7 done

---
+++ js/task-07.js
@@ -0,0 +1,15 @@
+
+
+const inputEl = document.querySelector('#font-size-control');
+const spanEl = document.querySelector('#text');
+
+const onInput = event => {
+    const spanFontSize = event.currentTarget.value;
+
+    spanEl.style.fontSize = `${spanFontSize}px`
+};
+
+inputEl.addEventListener('input', onInput)
+
+
+
