0ccb10233068c1cce05180c87c2355f91ebec302
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-08-20 14:29:11 +0300

    Task-9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+console.log(getRandomHexColor())
+
+const body = document.querySelector('body')
+console.log(body)
+const span = document.querySelector('span')
+console.log(span)
+const button = document.querySelector('button')
+console.log(button)
+
+const onButtonClick = () => {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  span.textContent = `${getRandomHexColor()}`;
+}
+
+button.addEventListener('click', onButtonClick)
